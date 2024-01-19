3b72f2de2927428dd4c520008ff46bdd19a61734
idmed22 <idlmhor.med@gmail.com>
2024-01-19 11:08:52 +0100

    add profile person component

---
+++ src/app/modules/shipping/delivery-person/profile-person/profile-person.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilePersonComponent } from './profile-person.component';
+
+describe('ProfilePersonComponent', () => {
+  let component: ProfilePersonComponent;
+  let fixture: ComponentFixture<ProfilePersonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilePersonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfilePersonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
